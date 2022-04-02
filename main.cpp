be2841fcb2dc81fc632007f4ce70738ad62965cb
AlbertoCarena <100611068+AlbertoCarena@users.noreply.github.com>
2022-04-02 20:27:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   int n,a=1,b=1,c,d=3;
+   cin>>n;
+   if(n>=2){
+      cout<< "1" <<endl;
+      cout<< "1" <<endl;
+      while(n>=d) {
+         c = a + b;
+         cout << c << endl;
+         b=a;
+         a=c;
+         d=d+1;
+      }
+   }
+   else if (n<2){
+      cout << "errore" <<endl;
+   } 
    return 0;
 }
